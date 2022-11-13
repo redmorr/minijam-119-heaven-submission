e0ebf4744b85b4a1890c4efb857837397411827d
redmorr <19555512+redmorr@users.noreply.github.com>
2022-11-13 21:41:30 +0100

    New tiles; New physics; More ammo

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,32 +4,45 @@ using UnityEngine.Tilemaps;
 public class Bullet : MonoBehaviour
 {
     public int Damage;
+    public int Health;
     public float Speed = 10f;
     public float PushbackForce;
 
     private DestructableTilemap tilemap;
     private Health health;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     public void Shoot()
-    {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        
+    {   
         rb.AddForce(transform.right * Speed, ForceMode2D.Impulse);
+        Destroy(gameObject, 2f);
     }
+
+    public
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.TryGetComponent(out tilemap))
         {
             tilemap.DestroyTiles(collision);
+            Health--;
+            if (Health <= 0)
+                Destroy(gameObject);
+            //rb.AddForce(transform.right * Speed, ForceMode2D.Impulse);
         }
 
         if (collision.collider.TryGetComponent(out health))
         {
             health.Damage(Damage, PushbackForce * transform.right);
+            Destroy(gameObject);
         }
 
-        Destroy(gameObject);
+        
     }
 
     //void OnTriggerEnter2D(Collider2D collider)
